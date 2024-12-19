5cefa2b59954303324cd70a6a7334a09168d40b9
omhpeinture <omhpeinture01@gmail.com>
2024-12-19 03:01:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Récupère le header
-const header = document.getElementById("header");
-
-// Écoute l'événement de défilement
-window.addEventListener("scroll", function() {
-    // Si l'utilisateur a défilé plus de 50px
-    if (window.scrollY > 50) {
-        header.classList.add("scrolled"); // Ajoute la classe 'scrolled'
-    } else {
-        header.classList.remove("scrolled"); // Retire la classe 'scrolled'
-    }
-});
 
